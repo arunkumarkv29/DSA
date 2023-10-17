896cae8127c5424a7b7d13311e39f9933f31bca0
Arun Kumar Sharma <arunkumarkv29@gmail.com>
2023-10-17 11:58:00 +0530

    deepcopy code

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,27 @@
+import javax.swing.undo.AbstractUndoableEdit;
 import java.util.concurrent.*;
 
 public class Main {
+    public static class Student {
+        String name ;
+        int rollNo ;
+        int age ;
+        Student(String name, int rollNo, int age){
+            this.name = name ;
+            this.rollNo = rollNo ;
+            this.age = age ;
+        }
+        Student(Student student){
+            this.name = student.name ;
+            this.rollNo = student.rollNo ;
+            this.age = student.age ;
+        }
+    }
     public static void main(String[] args) {
-        int [] A = {5,8,1,15,7,6,2} ;
-        int N = A.length ;
-
-        for(int i=0 ; i<N ; i++){
-            for(int j=1; j<N-i ; j++){
-                if(A[j-1]>A[j]){
-                    int temp = A[j-1] ;
-                    A[j-1] = A[j] ;
-                    A[j] = temp ;
-                }
-            }
-        }
-
-        for(int i=0 ; i<N; i++){
-            System.out.print(A[i] + " ");
-        }
+        Student S1 = new Student("Arun",12345,29) ;
+        Student S2 = new Student((S1)) ;
+        System.out.println(S2.name);
+        System.out.println(S2.age);
+        System.out.println(S2.rollNo);
     }
 }
