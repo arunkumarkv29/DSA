31843593a0a7570a5ff6fc6ab34c3f35fd1dc932
Arun Kumar Sharma <arunkumarkv29@gmail.com>
2023-10-17 21:00:44 +0530

    merge sorting

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,71 @@
 import javax.swing.undo.AbstractUndoableEdit;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.concurrent.*;
 
 public class Main {
-    public static class Student {
-        String name ;
-        int rollNo ;
-        int age ;
-        Student(String name, int rollNo, int age){
-            this.name = name ;
-            this.rollNo = rollNo ;
-            this.age = age ;
+    public static void Merge(int[] A , int s , int mid, int e){
+        int N = mid-s+1 ;
+        int M = e-mid ;
+        int[] first = new int[N] ;
+        int[] second = new int[M] ;
+        int i=0 ;
+        int j=0 ;
+        int k=s ;
+        while(i<N){
+            first[i] = A[k] ;
+            i++ ;
+            k++ ;
         }
-        Student(Student student){
-            this.name = student.name ;
-            this.rollNo = student.rollNo ;
-            this.age = student.age ;
+        k=mid+1 ;
+        while(j<M){
+            second[j] = A[k] ;
+            j++ ;
+            k++ ;
+        }
+        i=0 ;j=0 ; k=s ;
+        while(i<N && j<M){
+            if(first[i]<second[j]){
+                A[k] = first[i] ;
+                i++ ;
+                k++ ;
+            }
+            else{
+                A[k] = second[j] ;
+                j++ ;
+                k++ ;
+            }
+        }
+        while(i<N){
+            A[k] = first[i] ;
+            i++ ;
+            k++ ;
+        }
+        while(j<M){
+            A[k] = second[j] ;
+            j++ ;
+            k++ ;
         }
     }
+    public static void Divide(int[] A, int s , int e){
+        if(s>=e){
+            return ;
+        }
+
+        int mid = (s+e)/2 ;
+
+        Divide(A,s,mid);
+        Divide(A,mid+1,e);
+        Merge(A,s,mid,e) ;
+    }
     public static void main(String[] args) {
-        Student S1 = new Student("Arun",12345,29) ;
-        Student S2 = new Student((S1)) ;
-        System.out.println(S2.name);
-        System.out.println(S2.age);
-        System.out.println(S2.rollNo);
+        int[] A = {38,27,43,3,9,82,10} ;
+        int N = A.length ;
+        Divide(A,0,N-1);
+        for(int i=0 ; i<N;  i++){
+            System.out.print(A[i]+" ");
+        }
     }
 }
